c99158bf4606a3cd9d1537b16d9466dea9c84d78
yanghojoon <yanghojoon8487@gmail.com>
2022-01-29 19:09:32 +0900

    Initial Commit

---
+++ WeekPracticeCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeekPracticeCoreData
+//
+//  Created by 양호준 on 2022/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
