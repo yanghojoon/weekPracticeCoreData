67b1a21d10a48c842d320bc9155623b8138b7d13
yanghojoon <yanghojoon8487@gmail.com>
2022-01-29 20:14:44 +0900

    feat: 뷰가 다시 나타나면 데이터를 reload 하도록 구현

--- WeekPracticeCoreData/ViewController.swift
+++ WeekPracticeCoreData/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 }
 
 extension ViewController: UITableViewDataSource {
